bccb1bc39e2cacd06d12f6550d3a35484bf99961
Cuong Ngo <cuong.ngo@ascendcorp.com>
2018-04-22 12:26:34 +0700

    Authentication

--- src/main/java/com/codeko/spring5reactive/bootstraps/DataInitializer.java
+++ src/main/java/com/codeko/spring5reactive/bootstraps/DataInitializer.java
@@ -1,26 +1,66 @@
 package com.codeko.spring5reactive.bootstraps;
 
 import com.codeko.spring5reactive.models.Post;
+import com.codeko.spring5reactive.models.User;
 import com.codeko.spring5reactive.repositories.PostRepository;
+import com.codeko.spring5reactive.repositories.UserRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Flux;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Component
 @Slf4j
 public class DataInitializer {
 
     private PostRepository postRepository;
+    private UserRepository userRepository;
+    private PasswordEncoder passwordEncoder;
 
-    public DataInitializer(PostRepository postRepository) {
+    public DataInitializer(PostRepository postRepository, UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.postRepository = postRepository;
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @EventListener(value = ContextRefreshedEvent.class)
     public void init() {
         initPosts();
+        initUsers();
+    }
+
+    private void initUsers() {
+        this.userRepository
+                .deleteAll()
+                .thenMany(
+                        Flux
+                                .just("user", "admin")
+                                .flatMap(
+                                        username -> {
+                                            List<String> roles = "user".equals(username)
+                                                    ? Arrays.asList("ROLE_USER")
+                                                    : Arrays.asList("ROLE_USER", "ROLE_ADMIN");
+                                            User user = User.builder()
+                                                    .roles(roles)
+                                                    .username(username)
+                                                    .password(passwordEncoder.encode("password"))
+                                                    .email(username + "@example.com")
+                                                    .build();
+                                            return this.userRepository.save(user);
+                                        }
+                                )
+                )
+                .log()
+                .subscribe(
+                        null,
+                        null,
+                        () -> log.info("done users initialization...")
+                );
     }
 
     private void initPosts() {
---
+++ src/main/java/com/codeko/spring5reactive/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.codeko.spring5reactive.repositories;
+
+import com.codeko.spring5reactive.models.User;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import reactor.core.publisher.Mono;
+
+public interface UserRepository extends ReactiveMongoRepository<User, String> {
+    Mono<User> findByUsername(String username);
+}
