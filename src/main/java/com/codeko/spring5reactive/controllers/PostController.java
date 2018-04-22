61236bf39e57dd5b1048888e4115bf729b917fea
Cuong Ngo <cuong.ngo@ascendcorp.com>
2018-04-22 11:55:31 +0700

    Create, get and update specific post

--- src/main/java/com/codeko/spring5reactive/controllers/PostController.java
+++ src/main/java/com/codeko/spring5reactive/controllers/PostController.java
@@ -2,10 +2,18 @@ package com.codeko.spring5reactive.controllers;
 
 import com.codeko.spring5reactive.models.Post;
 import com.codeko.spring5reactive.repositories.PostRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/posts")
@@ -22,5 +30,33 @@ public class PostController {
         return this.postRepository.findAll();
     }
 
+    @PostMapping("")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Mono<Post> createPost(@RequestBody Post post) {
+        return this.postRepository.save(post);
+    }
 
+    @GetMapping("/{id}")
+    public Mono<Post> getPostById(@PathVariable String id) {
+        return this.postRepository.findById(id);
+    }
+
+    @PutMapping("/{id}")
+    public Mono<Post> updatePost(@PathVariable String id, @RequestBody Post post) {
+        return this.postRepository
+                .findById(id)
+                .map(p -> {
+                    p.setContent(post.getContent());
+                    p.setTitle(post.getTitle());
+
+                    return p;
+                })
+                .flatMap(p -> this.postRepository.save(p));
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public Mono<Void> deletePostById(@PathVariable String id) {
+        return this.postRepository.deleteById(id);
+    }
 }
